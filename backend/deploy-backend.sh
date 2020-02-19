269a92d50e3603e519136865ea7426a64574c550
piercecave <piercecave@yahoo.com>
2020-02-18 19:59:18 -0800

    updated deploy scripts

--- backend/deploy-backend.sh
+++ backend/deploy-backend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DOCKERNAME=piercecave
+export DOCKERNAME=$(head -n 1 ./../docker_username)
 
 docker build -t $DOCKERNAME/skilledgegateway ./gateway/
 docker build -t $DOCKERNAME/skilledgedatabase ./db/
@@ -9,5 +9,5 @@ docker push $DOCKERNAME/skilledgegateway
 docker push $DOCKERNAME/skilledgedatabase
 echo "✅  Push Docker Containers to DockerHub"
 
-ssh -i ./../shared_project_key_serverside.pem -oStrictHostKeyChecking=no ec2-user@api.skilledge.site 'bash -s' < update-servers.sh $DOCKERNAME
+ssh -i ./../shared_project_key_serverside.pem -oStrictHostKeyChecking=no ec2-user@api.skilledge.site 'bash -s' < update-backend.sh $DOCKERNAME
 echo "🎊  Server Deployment Complete!"
--- client/deploy-client.sh
+++ client/deploy-client.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export DOCKERNAME=piercecave
+export DOCKERNAME=$(head -n 1 ./../docker_username)
 
 docker build -t piercecave/pong_client .
 echo "✅  Local Docker Build Complete"
 docker push piercecave/pong_client
 echo "✅  Local Docker Push Complete"
-ssh -i ./../shared_project_key.pem -oStrictHostKeyChecking=no ec2-user@skilledge.site 'bash -s' < upgrade-server.sh $DOCKERNAME
+ssh -i ./../shared_project_key.pem -oStrictHostKeyChecking=no ec2-user@skilledge.site 'bash -s' < upgrade-client.sh $DOCKERNAME
 echo "🎊  Client Deployment Complete!"
 read -p "Press any key..."
