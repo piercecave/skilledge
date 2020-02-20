47be5e87b9d3b429c3ff1497a4b1754956d0077d
piercecave <piercecave@yahoo.com>
2020-02-20 00:15:56 -0800

    make nav bar be loaded by js for consistency

---
+++ client/header/insert_header.js
@@ -0,0 +1,35 @@
+"use strict;"
+
+document.getElementById("main_nav").innerHTML = `
+<nav id="navbar" class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="./../">SKILLEDGE</a>
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="./../">Home<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="./../sign_up/">Sign Up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./../log_in/">Log In</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./../choose_skill/">Choose Skill</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./../set_up_habit/">Set Up Habit</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./../record/">Record</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./../calendar/">Calendar</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="./../charts/">Charts</a>
+      </li>
+    </ul>
+  </nav>
+`
