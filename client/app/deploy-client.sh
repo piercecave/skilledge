090895f7d8e17c05419197f9edcd5ddc07113692
piercecave <piercecave@yahoo.com>
2020-04-09 00:41:53 -0700

    deploys to aws!

---
+++ client/app/deploy-client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export DOCKERNAME=$(head -n 1 ./../../docker_username)
+
+docker build -t $DOCKERNAME/skilledge_client .
+echo "✅  Local Docker Build Complete"
+docker push $DOCKERNAME/skilledge_client
+echo "✅  Local Docker Push Complete"
+ssh -i ./../../shared_project_key.pem -oStrictHostKeyChecking=no ec2-user@skilledge.site 'bash -s' < upgrade-client.sh $DOCKERNAME
+echo "🎊  Client Deployment Complete!"
+read -p "Press any key..."
