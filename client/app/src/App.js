6613727ff3caab5a368121aef5a87ee7cd3bb3fa
piercecave <piercecave@yahoo.com>
2020-04-10 22:50:00 -0700

    started on chooseskill

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -14,13 +14,13 @@ function App() {
   return (
     <BrowserRouter>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/set_up_habit/' component={SetUpHabitPage} />
-      <Route exact path='/calendar/' component={Calendar} />
       <Route exact path='/sign_up/' component={SignUp} />
-      <Route exact path='/record/' component={Record} />
       <Route exact path='/log_in/' component={Login} />
       <Route exact path='/log_out/' component={Logout} />
       <Route exact path='/choose_skill/' component={ChooseSkill} />
+      <Route exact path='/set_up_habit/' component={SetUpHabitPage} />
+      <Route exact path='/calendar/' component={Calendar} />
+      <Route exact path='/record/' component={Record} />
     </BrowserRouter>
   );
 }
--- client/app/src/ChooseSkill.js
+++ client/app/src/ChooseSkill.js
@@ -1,35 +1,109 @@
 import React from 'react';
-// import './HomePage.css';
-import { Header } from './Header'; 
+import './ChooseSkill.css';
+import { Header } from './Header';
 
 export class ChooseSkill extends React.Component {
-    render() {
-        return (
-            <div className="ChooseSkill">
-                <Header />
-                {renderScreen()}
-            </div>
-        );
-    }
-}
 
-function renderScreen() {
+  constructor(props) {
+    super(props);
+    this.GET_SKILLS_URL = process.env.REACT_APP_BACKEND_URL + "/skills";
+    this.ADD_SKILL_URL = process.env.REACT_APP_BACKEND_URL + "/users/skills/";
+    this.initChooseSkill = this.initChooseSkill.bind(this);
+  }
+
+  initChooseSkill() {
+    this.checkAuthenticated();
+  };
+
+  checkAuthenticated() {
+    fetch(this.GET_SKILLS_URL, {
+      credentials: 'include'
+    })
+      .then(this.displayResult)
+      .catch(this.displayError);
+  }
+
+  async displayResult(response) {
+    const responseJSON = await response.json();
+    console.log(responseJSON);
+
+    const skillsContainer = document.getElementById('skillsContainer');
+
+    for (const skill of responseJSON) {
+
+      // Create card
+      const skillCard = document.createElement("div");
+      skillCard.classList.add("card", "my-3");
+      skillsContainer.appendChild(skillCard);
+      // Create card body
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+      skillCard.appendChild(cardBody);
+      // Create label
+      const cardTitle = document.createElement("h5");
+      cardTitle.classList.add("card-title");
+      cardTitle.innerText = skill.SkillName;
+      cardBody.appendChild(cardTitle);
+      // Create action
+      const cardText = document.createElement("p");
+      cardText.classList.add("card-text");
+      cardText.innerText = skill.SkillDesc;
+      cardBody.appendChild(cardText);
+      // Create submit button
+      const skillButton = document.createElement("button");
+      skillButton.classList.add("btn", "btn-primary");
+      skillButton.innerText = "Select";
+      cardBody.appendChild(skillButton);
+
+      skillButton.onclick = () => {
+        fetch(this.ADD_SKILL_URL + skill.SkillID, {
+          method: 'POST',
+          credentials: 'include'
+        })
+          .then((response) => {
+            skillButton.setAttribute("disabled", "n/a");
+            window.location.href = '/set_up_habit';
+          })
+          .catch(this.displayError);
+      };
+    }
+  }
+
+  displayError(error) {
+    console.log(error);
+  }
+
+  id(idName) {
+    return document.getElementById(idName);
+  }
+
+  idValue(idName) {
+    return document.getElementById(idName).value;
+  }
+
+  checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      return Promise.reject(new Error(response.status + ": " + response.statusText));
+    }
+  }
+
+  render() {
     return (
-        <body>
-  <div id="main_nav"></div>
-  <main>
-    <div id="chooseSkillContainer" class="container">
-      <div class="card my-3">
-        <div class="card-body">
-          <h5 class="card-title">Choose Skill:</h5>
-          <p><em>(We'll help you figure out how next!)</em></p>
-          <div id="skillsContainer" class="container my-3"></div>
+      <div className="ChooseSkill">
+        <Header />
+        <div id="chooseSkillContainer" className="container">
+          <div className="card my-3">
+            <div className="card-body">
+              <h5 className="card-title">Choose Skill:</h5>
+              <p><em>(We'll help you figure out how next!)</em></p>
+              <div id="skillsContainer" className="container my-3"></div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </main>
-</body>
-    )
+    );
+  }
 }
-
 export default ChooseSkill;
