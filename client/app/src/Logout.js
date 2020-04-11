0bcb619123c561e2d56a5ba74b1d23e0b3a0deea
piercecave <piercecave@yahoo.com>
2020-04-10 21:45:47 -0700

    log in and out working

--- client/app/src/Logout.js
+++ client/app/src/Logout.js
@@ -1,34 +1,84 @@
 import React from 'react';
-// import './HomePage.css';
-import { Header } from './Header'; 
+import './Logout.css';
+import { Header } from './Header';
 
 export class Logout extends React.Component {
-    render() {
-        return (
-            <div className="Logout">
-                <Header />
-                { renderScreen() }
-            </div>
-        );
-    }
-}
 
-function renderScreen() {
+  constructor(props) {
+    super(props);
+    this.BASE_URL = process.env.REACT_APP_BACKEND_URL + "/sessions";
+    this.initLogOut = this.initLogOut.bind(this);
+  }
+
+  initLogOut(event) {
+    event.preventDefault();
+    this.logOut();
+  }
+
+  logOut() {
+    fetch(this.BASE_URL, {
+      method: 'DELETE',
+      credentials: 'include'
+    }).then(this.checkStatus)
+      .then(this.redirect)
+      .catch(this.displayError)
+  }
+
+  redirect() {
+    window.location = "./../";
+  }
+
+  displayError(error) {
+    // Retrieve container for displaying error
+    console.log(error);
+  }
+
+  id(idName) {
+    return document.getElementById(idName);
+  }
+
+  idValue(idName) {
+    return document.getElementById(idName).value;
+  }
+
+  checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      return Promise.reject(new Error(response.status + ": " + response.statusText));
+    }
+  }
+
+  getAuthToken() {
+    let nameEQ = "auth=";
+    let cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i];
+      while (cookie.charAt(0) == " ") {
+        cookie = cookie.substring(1, cookie.length);
+      }
+      if (cookie.indexOf(nameEQ) == 0) {
+        return cookie.substring(nameEQ.length, cookie.length);
+      }
+    }
+    return null;
+  }
+
+  render() {
     return (
-<body>
-  <div id="main_nav"></div>
-  <main>
-    <section id="login-form">
-      <div class="page-header">
-        <h1>Log Out?</h1>
+      <div className="Logout">
+        <Header />
+        <section id="login-form">
+          <div class="page-header">
+            <h1>Log Out?</h1>
+          </div>
+          <br />
+          <button id="log_out" class="btn btn-primary" onClick={this.initLogOut}>Log Out</button>
+          <div id="meta-container" class="card hidden"></div>
+        </section>
       </div>
-      <br/>
-      <button id="log_out" class="btn btn-primary">Log Out</button>
-      <div id="meta-container" class="card hidden"></div>
-    </section>
-  </main>
-</body>
-    )
+    );
+  }
 }
 
 export default Logout;
